e3b347f923a3c787341788f7ab95f221b87901d8
nullstead <nullstead@outlook.com>
2024-11-13 19:40:47 +0000

    a little clean-up

--- tests/demo.js
+++
@@ -1,7 +0,0 @@
-const { HumanFriendlyDate } = require('../src');
-
-const any_timestamp = new Date().getTime() + 3153000000;
-
-const result = HumanFriendlyDate(any_timestamp, 'en');
-
-console.log(result)
